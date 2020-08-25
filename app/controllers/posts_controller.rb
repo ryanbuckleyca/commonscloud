d686b5ab4df10deb916ffb18e024e25670419a18
Ryan Buckley <ryanbuckley@gmail.com>
2020-08-25 19:46:37 -0400

    seed and complete controller actions to test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
@@ -9,5 +10,6 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  belongs_to :posts
+  has_many :posts
 end
