963831b4d44cb024a1bb2bfa7372ff664edcdb6e
Ryan Buckley <ryanbuckley@gmail.com>
2020-08-24 20:10:20 -0400

    added db references

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :category
   belongs_to :user
+  has_many :connections, dependent: :destroy
+  has_many :users, through: :connections
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :connections, dependent: :destroy
+  has_many :posts, through: :connections
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
