eb9dfff6f3d10bc584b66976efe78c98f4dcb681
Ryan Buckley <ryanbuckley@gmail.com>
2020-08-25 15:46:08 -0400

    added three columns to user table by editing device_create migration

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,9 @@ ActiveRecord::Schema.define(version: 2020_08_24_235316) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "address", default: "", null: false
+    t.string "phone", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
