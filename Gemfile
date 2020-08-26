647ebf0383de2592b51d35e40a7ed22387c17b70
deb-anjos <debora_anjos@icloud.com>
2020-08-26 11:46:22 -0400

    geocoder added to db and post model

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'devise'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
+gem 'geocoder'
+
 group :development, :test do  gem 'pry-byebug'
   gem 'pry-rails'
   gem 'dotenv-rails'
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
   has_many :connections, dependent: :destroy
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
