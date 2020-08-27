221926dd33547d11a566ea282348f8eeb6e050a0
arthur prats <arthurprats@gmail.com>
2020-08-27 14:36:03 -0400

    this is step1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,9 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
 
+import { scrollup } from '../plugins/scroll_up';
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  scrollup();
 });
---
+++ app/javascript/plugins/scroll_up.js
@@ -0,0 +1,5 @@
+const scrollup = () => {
+
+}
+
+export {scrollup}
