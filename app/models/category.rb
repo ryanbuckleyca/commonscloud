e7937646b02e0d76ca6c3f24efb51922e924ab25
Ryan Buckley <ryanbuckley@gmail.com>
2020-08-28 15:53:58 -0400

    added dynamic tabs for dashboard content

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,24 @@
 class Category < ApplicationRecord
   has_many :posts
+
+  def icon
+    case name
+    when "tools" then 'fas fa-tools'
+    when "tech help" then 'fas fa-desktop'
+    when "medicine" then 'fas fa-heartbeat'
+    when "ppe" then 'fas fa-head-side-mask'
+    when "hygiene supplies" then 'fas fa-hands-wash'
+    when "food" then 'fas fa-apple-alt'
+    when "transportation" then 'fas fa-shuttle-van'
+    when "errands" then 'fas fa-running'
+    when "deliveries" then 'fas fa-dolly'
+    when "clothing" then 'fas fa-tshirt'
+    when "financial support" then 'fas fa-donate'
+    when "school supplies" then 'fas fa-graduation-cap'
+    when "shelter" then 'fas fa-house-user'
+    when "cleaning supplies" then 'fas fa-quidditch'
+    when "household supplies" then 'fas fa-chair'
+    when "emotional support" then 'fas fa-comments'
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -16,24 +16,7 @@ class Post < ApplicationRecord
   end
 
   def icon
-    case category.name
-    when "tools" then 'fas fa-tools'
-    when "tech help" then 'fas fa-desktop'
-    when "medicine" then 'fas fa-heartbeat'
-    when "ppe" then 'fas fa-head-side-mask'
-    when "hygiene supplies" then 'fas fa-hands-wash'
-    when "food" then 'fas fa-apple-alt'
-    when "transportation" then 'fas fa-shuttle-van'
-    when "errands" then 'fas fa-running'
-    when "deliveries" then 'fas fa-dolly'
-    when "clothing" then 'fas fa-tshirt'
-    when "financial support" then 'fas fa-donate'
-    when "school supplies" then 'fas fa-graduation-cap'
-    when "shelter" then 'fas fa-house-user'
-    when "cleaning supplies" then 'fas fa-quidditch'
-    when "household supplies" then 'fas fa-chair'
-    when "emotional support" then 'fas fa-comments'
-    end
+    category.icon
   end
 
   geocoded_by :location
