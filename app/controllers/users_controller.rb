1780868d9219250a84954f652b6c7303625cb450
deb-anjos <debora_anjos@icloud.com>
2020-08-28 14:15:38 -0400

    dashboard backend completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,8 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @posts = Post.where(author_id: current_user.id)
+    @post_connection = @posts.connection
+    @user_connections = Connection.where(responder_id: current_user.id)
+
   end
 end
