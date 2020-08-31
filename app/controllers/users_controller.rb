824fa660fe8a9a2aeefd224e6bb98b84eae98c22
deb-anjos <debora_anjos@icloud.com>
2020-08-31 16:24:05 -0400

    user lands on the correct tab after creating a post or a connection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,12 @@ class UsersController < ApplicationController
   end
 
   def show
+    if params[:tab] == "outgoing" || params[:tab] == "myposts"
+      @tab = params[:tab]
+    else
+      @tab = "incoming"
+    end
+
     @user = current_user
     @posts = Post.where(author_id: current_user.id).order('created_at DESC')
     @user_connections = Connection.where(responder_id: current_user.id).order('created_at DESC')
