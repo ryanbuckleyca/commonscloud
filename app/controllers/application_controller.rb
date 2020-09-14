b9fb95902f9bf82a5f7f965f93ccc3e72a0908df
Ryan Buckley <ryanbuckley@gmail.com>
2020-09-14 14:49:24 -0400

    update application_controller with new user fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :address, :img_url, :phone, :password) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :address, :img_url, :phone, :password, :current_password) }
   end
 end
